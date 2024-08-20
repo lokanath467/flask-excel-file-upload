91c72a365bd40c1416b86f29bb3dfa4ce201e40d
Lokanath M <Lokanath467@gmail.com>
2024-08-20 11:39:59 +0530

    Initial project skeleton1

--- app/app/main.py
+++ app/app/main.py
@@ -1,23 +1,24 @@
-
 from flask import Flask
 from flask import request
 import pandas as pd
 from flask_cors import CORS
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://postgres:123@localhost:5432/helloworld')
+
+engine = create_engine("postgresql://postgres:123@localhost:5432/helloworld")
 
 app = Flask(__name__)
 CORS(app)
 
-conn=engine.connect()
+conn = engine.connect()
 
-@app.route('/upload_excel',methods=["POST"])
+
+@app.route("/upload_excel", methods=["POST"])
 def UploadExcel():
-    f=request.files['upload_excel']
-    data_xls=pd.read_excel(f)
-    data_xls.to_sql('data',con=conn,if_exists='replace',index=False)
+    f = request.files["upload_excel"]
+    data_xls = pd.read_excel(f)
+    data_xls.to_sql("data", con=conn, if_exists="replace", index=False)
     return data_xls.to_json()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
